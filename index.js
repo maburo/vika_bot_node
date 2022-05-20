a1ab82195f5dacbf94ae602034881d5fdf7b6eb8
alex <galex.rus@gmail.com>
2022-05-20 20:42:20 +0300

    wip

--- index.js
+++ index.js
@@ -1,57 +1,125 @@
 import express from "express";
 import bodyParser from "body-parser";
-import fetch from 'node-fetch';
-// import axios from "axios";
+import axios from "axios";
+import states from './states.js';
+
 
 const port = process.env.PORT || 3001;
 const token = process.env.TOKEN || '';
-const baseUrl = process.env.APP_URL || 'https://8aba-178-66-158-184.eu.ngrok.io'
+const baseUrl = process.env.APP_URL || 'https://1616-178-66-158-184.eu.ngrok.io'
 
 const hookPath = "telegram/hook"
 const telegramUrl = `https://api.telegram.org/bot${token}`
+
+const users = new Map();
+function getUser(message) {
+    const user = users.get(message.from.username);
+    if (!user) {
+        const tmp = {
+            username: message.from.username,
+            state: states.find(s => s.name === 'start')
+        };
+        users.set(message.from.username, tmp);
+        return tmp;
+    }
+
+    return user
+}
+
+const photos = new Map();
+export function getPhoto(name) {
+    const url = `${baseUrl}/assets/${name}`;
+    return photos.get(name) || url;
+}
 
 setWebhook()
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
+app.get("/", (req, res) => {    
     res.send("ok")
 });
 
+app.use('/assets', express.static('./assets'))
+
 app.post(`/${hookPath}`, (req, res) => {
-    console.log(req.body);
+    const body = req.body
+    console.log(body);
+    const chatId = body.message.chat.id
+
+    const user = getUser(body.message);
+    const context = {
+        chatId,
+        user
+    };
+
+    let nextStateName = user.state.nextState(body, context);
+    while (nextStateName) {
+        const nextState = states.find(s => s.name === nextStateName)
+
+        nextStateName = nextState.action(context);
+        user.prevState = user.state
+        user.state = nextState
+    }
+
+    console.log(`chat id ${chatId}`);
+
+    res.sendStatus(200)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-axios.interceptors.request.use(cfg => {
-    console.log(cfg);
-    return cfg
-})
-
 async function setWebhook() {
     const hook = `${telegramUrl}/setWebhook`
-    // axios.post(hook, {
-    //     url: `${baseUrl}/${hookPath}`
-    // })
-    // .then(resp => {
-    //     console.log(resp.data);
-    // })
-    // .catch(err => {
-    //     console.log(err.data);
-        // for (const k in err.request) {
-        //     console.log(k);
-        // }
-        // console.log(err.response.data);
-    // })
-    // console.log(hook);
-    const resp = await fetch(hook, {
-        method: 'POST', 
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({url: `${baseUrl}/${hookPath}`})
+    await axios.post(hook, {
+        url: `${baseUrl}/${hookPath}`
     })
-    console.log(await resp.json());
-    // console.log(resp.);
+    .then(resp => {
+        console.log(resp.data);
+    })
+    .catch(err => {
+        console.error(err.data);
+    })
 }
 
+export async function sendDefaultMessage(ctx) {
+    sendMessage({
+        chat_id: ctx.chatId,
+        text: 'Что что?'
+    });
+
+    console.log(`state: ${ctx.user.currentState.name}`)
+
+    ctx.user.state.action(ctx);
+
+    return ""
+}
+
+export async function sendMessage(message) {
+    await axios.post(`${telegramUrl}/sendMessage`, message)
+    .then(resp => console.log(resp.data))
+    .catch(err => console.error(err.data))
+}
+
+export async function sendDice(chatId) {
+    return await axios.post(`${telegramUrl}/sendDice`, {
+        chat_id: chatId,
+        emoji:  "\uD83C\uDFB2",
+        reply_markup: { remove_keyboard: true }
+    })
+    .then(resp => console.log(resp.data))
+    .catch(err => console.error(err.data))
+}
+
+export async function sendPhoto(message) {
+    return await axios.post(`${telegramUrl}/sendPhoto`, message)
+    then(resp => {
+        // resp.data.photo[resp.data.photo.length - 1]
+        // response.body<Message>().photo?.last()?.let {
+        //     photos.put(message.photo, it.file_id)
+        // }
+    })
+    .catch(err => console.error(err.data))
+
+}
---
+++ states.js
@@ -0,0 +1,179 @@
+import { sendMessage, sendPhoto, sendDice, sendDefaultMessage, getPhoto } from './index.js';
+
+function keyboard(...keys) {
+    return { keyboard: [[...keys]] };
+}
+
+function inlineKeyboard(...keys) {
+    return { inline_keyboard: [[...keys]] };
+}
+
+const TEST_YES_BTN = "Это мне близко и понятно"
+const TEST_NO_BTN = "Это мне не нравится"
+const TEST_KEYS = keyboard(TEST_YES_BTN, TEST_NO_BTN);
+function testState(name, img, nextState) {
+    return {
+        name,
+        action: ctx => {
+            sendPhoto({
+                chat_id: ctx.chatId,
+                photo: getPhoto(img),
+                caption: "Как вам это?",
+                reply_markup: TEST_KEYS
+            });
+        },
+        nextState: (action, ctx) => {
+            switch (action.message?.text) {
+                case TEST_YES_BTN:
+                    ctx.user.testResult += "Y";
+                    return nextState;
+                
+                case TEST_NO_BTN:
+                    ctx.user.testResult += "N";
+                    return nextState;
+                default:
+                    return sendDefaultMessage(ctx);
+            }
+        }
+    };
+}
+
+function eventState(name, text) {
+    return {
+        name,
+        action: ctx => {
+            sendMessage({
+                chat_id: ctx.chatId,
+                text,
+                reply_markup: RemoveKeyboard()
+            });
+
+            sendPhoto({
+                chat_id: ctx.chatId,
+                photo: getPhoto("event.jpg"),
+                reply_markup: inlineKeyboard({text: "Перейти на сайт", url: "https://www.afisha.ru/"})                
+            });
+
+            return "restart"
+        },
+        nextState: () => "restart"
+    }
+}
+
+const states = [
+    {
+        name: "start",
+        nextState: () => "welcome",
+    },
+    {
+        name: "welcome",
+        action: (ctx) => {
+            ctx.user.event = -1;
+            ctx.user.testResult = "";
+
+            sendMessage(
+                {
+                    chat_id: ctx.chatId,
+                    text: "Привет.\n" +
+                            "Я бот мобильного приложения Art & Freedom. Оно позволяет легко находить выставки современного искусства, которые подойдут именно тебе, а еще помогает найти компанию для похода на выставку. \n" +
+                            "Пока мои неспешные разработчики готовят к выходу приложение, я в меру своих возможностей, проведу тебя через лабиринты современного искусства к событиям, которые всколыхнут твое сердечко. ",
+                    reply_markup: keyboard("Найти компанию", "Выбрать событие")
+                }
+            );
+        },
+        nextState: (action, ctx) => {
+            switch (action.message.text) {
+                case "Найти компанию": 
+                    return "companion"
+                case "Выбрать событие":
+                    if (ctx.user.testResult === '') return 'start-test'
+                    else return 'event'
+                default:
+                    return sendDefaultMessage(ctx)
+            }
+        }
+    },
+    {
+        name: "start-test",
+        action: ctx => {
+            sendMessage({
+                text: "Мы знаем, что современное искусство бывает непонятным и сложным для восприятия. Но также оно позволяет быть порталом в мир личных чувств и переживаний. Ты совсем не обязан чувствовать, как все, когда соприкасаешься с ним. \n" +
+                        "Искусство обучает внутренней свободе. Диалогу с собой. Помогает задавать себе неудобные вопросы. \n" +
+                        "\n" +
+                        "Чтобы мне было проще подобрать события, которые подойдут именно тебе, пожалуйста, пройди тест, выбирая картинки, которые тебе откликаются. Это наш прообраз искусственного интеллекта, который в мобильном приложении будет находить для тебя события еще точнее. \n" +
+                        "\n" +
+                        "«Ныряем в искусство?» ",
+                chat_id: ctx.chatId,
+                reply_markup: keyboard("Да", "Нет")
+            })
+
+            null
+        },
+        nextState: (action, ctx) => {
+            switch (action.message.text) {
+                case "Да":
+                    return "test-block-1"
+                case "Нет":
+                    return "welcome"
+                default:
+                    return sendDefaultMessage(ctx)
+            }
+        }
+    },
+    testState("test-block-1", "test_1.jpg", "test-block-2"),
+    testState("test-block-2", "test_2.jpg", "test-block-3"),
+    testState("test-block-3", "test_3.jpg", "test-block-4"),
+    testState("test-block-4", "test_4.jpg", "event"),
+    {
+        name: "event",
+        action: ctx => {
+            sendMessage({
+                text: "Теперь я стал понимать тебя немного лучше и хочу предложить тебе некоторые события, которые в ближайшие дни будут проходить в Москве. Я предлагаю тебе градацию. Нажимая кнопку — точно да, ты увидишь три события, которые  скорее всего тебя порадуют. \n" +
+                        "Нажимая кнопку — точно нет, ты увидишь события, которые могут разочаровать и еще можешь проверить мою работу, и посмотреть вдруг я ошибся.\n" +
+                        "Нажимая кнопку — рискнуть, ты получишь рандомный список событий ",
+                reply_markup: keyboard("Точно да", "Точно нет", "Рискнуть"),
+                chat_id: ctx.chatId
+            });
+        },
+        nextState: (action, ctx) => {
+            switch (action.message.text) {
+                case "Точно да":
+                    return "good-event";
+                case "Точно нет":
+                    return "bad-event";
+                case "Рискнуть":
+                    return "random-event";
+                default: 
+                    return sendDefaultMessage(ctx);
+            }
+        }
+    },
+    eventState('good-event'),
+    eventState('bad-event'),
+    eventState('random-event'),
+    {
+        name: "restart",
+        action: ctx => {
+            sendMessage({
+                chat_id: ctx.chatId,
+                text: "Подходит?",
+                reply_markup: keyboard("Да", "Нет, хочу другое", "Пройти тест заново")
+            });
+        },
+        nextState: (action, ctx) => {
+            switch (action.message.text) {
+                case "Да":
+                    return "welcome";
+                case "Нет, хочу другое":
+                    return "event";
+                case "Пройти тест заново":
+                    ctx.user.testResult = "";
+                    return "start-test";
+                default:
+                    return sendDefaultMessage(ctx);
+            }
+        }
+    }
+];
+
+export default states;
